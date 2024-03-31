2ccf27a6d30fb572a7d1a7c0d23e6e0792080773
shastina03 <73755402+shastina03@users.noreply.github.com>
2024-03-31 16:29:48 +0300

    Задача выполнена

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-    Transaction transaction = null;
 
     public UserDaoHibernateImpl() {
 
